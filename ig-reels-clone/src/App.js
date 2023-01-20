04c3b8e3037e828d8e6d898d841190bd6fe00e65
Faizan <faizanalam980@gmail.com>
2023-01-20 22:23:31 +0530

    Set up the logo and header of the app, designed the video block too.

--- ig-reels-clone/src/App.js
+++ ig-reels-clone/src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      {/* <h1>This is IG-Reels Clone</h1> */}
+      <div className="app__top">
+      <img 
+      className="app__logo"
+      src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS6TNmrdCA6X6BCmHsmvC9wAUjim9md8lnJ0g&usqp=CAU" 
+      alt="" />
+      
+      <h1>Reels</h1>
+      </div>
+      
+      <div className="app__videos">
+      {/* Main Veritcally scrollable video container */}
+      </div>
     </div>
   );
 }
