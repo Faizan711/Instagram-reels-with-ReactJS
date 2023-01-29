7336a400a769d37b6d3b0c2a1c63190dd6a3a9af
Faizan <faizanalam980@gmail.com>
2023-01-29 20:03:38 +0530

    added react ticker

--- ig-reels-clone/src/VideoFooter.js
+++ ig-reels-clone/src/VideoFooter.js
@@ -1,9 +1,30 @@
+import { Avatar, Button } from '@mui/material'
+import MusicNoteIcon from '@mui/icons-material/MusicNote';
 import React from 'react'
 import './VideoFooter.css'
+import Ticker from 'react-ticker';
 
-function VideoFooter() {
+
+function VideoFooter({likes, shares, channel, avatarSrc, song}) {
   return (
-    <div>VideoFooter</div>
+    <div className='videoFooter'>
+        <div className='videoFooter_text'>
+            <Avatar src={avatarSrc}/>
+            <h3>
+                {channel} .<Button>Follow</Button>
+            </h3>
+        </div>
+        <div className='videoFooter_ticker'>
+            <MusicNoteIcon className='videoFooter_icon'/>
+            <Ticker mode='smooth'>
+                {({ index }) => (
+                    <>
+                        <h1>{song}</h1>
+                    </>
+                )}
+            </Ticker>
+        </div>
+    </div>
   )
 }
 
